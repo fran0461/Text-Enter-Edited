39e3751191d2f75b262036b61bf5ad9f631aec92
fran0461 <73315718+fran0461@users.noreply.github.com>
2021-03-12 11:09:42 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,16 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:io';
+import 'package:drop_cap_text/drop_cap_text.dart';
 import 'package:flutter/material.dart';
-
-import 'home.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'add_image.dart';
 //async
 void main()  {
   //WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp();
   //runApp(MaterialApp(
-   // home: MyApp(),
-   // debugShowCheckedModeBanner: false,
- // ));
+  // home: MyApp(),
+  // debugShowCheckedModeBanner: false,
+  // ));
   runApp(new MyApp(
     title: new Text("My App"), someText: new Text("Some Text..."),));
 }
@@ -25,22 +26,58 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   TextEditingController eCtrl = new TextEditingController();
   String value = "";
+
+  Future<File> imageFile;
+
+  pickImageFromGallery(ImageSource source){
+    setState(() {
+      imageFile = ImagePicker.pickImage(source: source);
+    });
+  }
+
+  Widget showImage(){
+    return FutureBuilder<File>(
+      future: imageFile,
+      builder: (BuildContext context, AsyncSnapshot<File> snapshot){
+        if(snapshot.connectionState == ConnectionState.done &&
+            snapshot.data !=null){
+          return Image.file(
+            snapshot.data,
+            width: 200,
+            height: 200,
+          );
+        }else if(snapshot.error != null){
+          return const Text(
+            'Error Picking Image',
+            textAlign: TextAlign.center,
+          );
+        }else{
+          return const Text(
+            'No Image Selected',
+            textAlign: TextAlign.center,
+          );
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-      appBar: new AppBar(title:widget.title,),
+        appBar: new AppBar(title:widget.title,),
         body: new Column(
           children: <Widget>[
-            //widget.someText,
             new TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
               controller: eCtrl,
               decoration: new InputDecoration.collapsed(hintText: "ADD SOMETHING"),
 
               onChanged: (String text){
-               setState(() {
-                 //value = text;
-               });
+                setState(() {
+                  //value = text;
+                });
               },
               onSubmitted: (String text){
                 setState(() {
@@ -49,11 +86,17 @@ class _MyAppState extends State<MyApp> {
                 eCtrl.clear();
               },
             ),
-            new Text("\n\n $value")
+            new Text("\n\n $value"),
+            showImage(),
+            RaisedButton(
+                child: Text("Select Image from Gallery"),
+                onPressed: (){
+                  pickImageFromGallery(ImageSource.gallery);
+                }
+            ),
           ],
         ),
       ),
-      );
-    //return HomePage();
+    );
   }
 }
